7b8e5a723088d0425c5d79ab477c216fbb1eecc6
Hans Christian Reinl <anything@drublic.de>
2012-09-23 23:27:15 +0200

    Highlight all code-sections with prism, detected by JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,16 @@
  * @author Hans Christian Reinl - @drublic
  *
  */
+
+(function ($) {
+
+	// Mark all code as language
+	var pre = $('pre'),
+		preLength, i, language;
+
+	for (preLength = pre.length, i = 0; i < preLength; i++) {
+		language = $(pre[i]).prev().children('span').attr('class');
+		$(pre[i]).addClass(language);
+	}
+
+}(Zepto));
