c42052ee135891d9d49656cae5c8c611946732da
Hans Christian Reinl <anything@drublic.de>
2012-11-16 21:10:29 +0100

    Add Disqus-feed to article single-view for comments
    Closes #15.

--- site/templates/default.php
+++ site/templates/default.php
@@ -2,4 +2,8 @@
 
 <?php snippet('article', array('article' => $page)) ?>
 
+<section class="comments">
+	<?php snippet('disqus', array('disqus_shortname' => 'thenittygritty')) ?>
+</section>
+
 <?php snippet('footer') ?>
