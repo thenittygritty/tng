04581d5a051dca2bfe9adab6f6e58745582d21dd
Hans Christian Reinl <anything@drublic.de>
2012-11-16 21:18:38 +0100

    Include GA tracking
    Closes #19.

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -10,5 +10,12 @@
 		<?php echo js('assets/js/vendor/zepto.min.js'); ?>
 		<?php echo js('assets/js/vendor/prism.js'); ?>
 		<?php echo js('assets/js/main.js'); ?>
+
+		<script>
+			var _gaq=[['_setAccount','UA-36398660-1'],['_trackPageview']];
+			(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+			g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+			s.parentNode.insertBefore(g,s)}(document,'script'));
+		</script>
 	</body>
 </html>
