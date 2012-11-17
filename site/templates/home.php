8de81381c4b6b1b177eebec21583c012ef3194c5
Kahlil Lechelt <kahlil@kahlil.co>
2012-11-17 14:36:35 +0100

    Add more test articles to test pagination

--- site/templates/home.php
+++ site/templates/home.php
@@ -5,20 +5,34 @@
 	// Do categories if requested
 	if (param('category')) {
 
-		$articles = $page->children()->visible()->flip()->filterBy('categories', param('category'), ',');
+		$articles = $page->children()->visible()->flip()->filterBy('categories', param('category'), ',')->paginate(5);
 		?>
 		<h1>Category: <?php echo param('category'); ?></h1>
 		<?php
 	} else {
 
 		// Do blog elsewise
-		$articles = $page->children()->visible()->flip();
+		$articles = $page->children()->visible()->flip()->paginate(5);
 	}
 ?>
 <?php if ($articles && $articles->count()): ?>
 	<?php foreach ($articles AS $article): ?>
 		<?php snippet('article', array('article' => $article)) ?>
 	<?php endforeach; ?>
+	<!-- Pagination -->
+	<?php if($articles->pagination()->hasPages()): ?>
+	<nav class="pagination">
+
+		<?php if($articles->pagination()->hasNextPage()): ?>
+		<a class="next" href="<?php echo $articles->pagination()->nextPageURL() ?>">&lsaquo; newer posts</a>
+		<?php endif ?>
+
+		<?php if($articles->pagination()->hasPrevPage()): ?>
+		<a class="prev" href="<?php echo $articles->pagination()->prevPageURL() ?>">older posts &rsaquo;</a>
+		<?php endif ?>
+
+	</nav>
+	<?php endif ?>
 <?php else: ?>
 	<article class="article">
 		<p>Wooops. We couldn't find any articles. *sadpanda*</h1>
