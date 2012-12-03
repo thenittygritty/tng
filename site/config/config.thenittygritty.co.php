a0ec020673570783021765e9d0cb59f9b5bad49d
Hans Christian Reinl <anything@drublic.de>
2012-12-03 01:14:44 +0100

    Set live-environment in config for live-url

---
+++ site/config/config.thenittygritty.co.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+
+---------------------------------------
+Debug
+---------------------------------------
+
+Set this to true to enable php errors.
+Make sure to keep this disabled for your
+production site, so you won't get nasty
+php errors there.
+
+*/
+
+c::set('debug', false);
