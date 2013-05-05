dac06661a8ab4bbb32b996f5beec1d0a738d2600
Kahlil Lechelt <kahlil@kahlil.co>
2013-05-05 14:50:58 +0200

    Use kirbytext parser

--- site/templates/content.php
+++ site/templates/content.php
@@ -5,7 +5,7 @@
 		<h1><?php echo html($page->title()) ?></h1>
 	</header>
 
-	<?php echo markdown($page->text()); ?>
+	<?php echo kirbytext($page->text()); ?>
 </article>
 
 <?php snippet('footer') ?>
