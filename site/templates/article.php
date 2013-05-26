7b2cc5d42ef8e66ea9115732209791bbb9ced2df
Kahlil Lechelt <kahlil@kahlil.co>
2013-05-26 17:16:10 +0200

    Add condition to exclude comments in articles

--- site/templates/article.php
+++ site/templates/article.php
@@ -2,8 +2,10 @@
 
 <?php snippet('article', array('article' => $page)) ?>
 
+<?php if ($page->comments() == 'on'): ?>
 <section class="comments">
 	<?php snippet('disqus', array('disqus_shortname' => 'thenittygritty')) ?>
 </section>
+<?php endif ?>
 
 <?php snippet('footer') ?>
