a63c7aed39e2d03ff823ac78ec31142f3302739f
Kahlil Lechelt <kahlil@kahlil.co>
2013-07-01 21:18:09 +0200

    bookmarklet changes

--- assets/js/bookmarklet.js
+++ assets/js/bookmarklet.js
@@ -14,7 +14,7 @@ if ( window[ MyNamespace ] ) {
 }
 window[ MyNamespace ] = {};
 
-var version = 1.3;
+var version = 1.6;
 var script  = document.createElement( 'script' );
 
 script.setAttribute( 'type', 'text/javascript' );
@@ -28,6 +28,7 @@ script.onload = script.onreadystatechange = function() {
         script.onload = script.onreadystatechange = null;
 
         // initialise or warn if older version
+        console.log(version, window[ MyNamespace ].version);
         if ( version !== window[ MyNamespace ].version ) {
             alert( 'This bookmarklet is out of date!' );
         } else {
@@ -39,4 +40,4 @@ script.onload = script.onreadystatechange = function() {
 
 
 
-javascript:(function(){var a="TNG";if(window[a])return window[a].init(),void 0;window[a]={};var b=1.6,c=document.createElement("script"),d=document.createElement("div"),e=document.createTextNode("Yo, traveller! The main script is loading. Please stand by...");d.id="tng-linkpost-wait",d.style.position="absolute",d.style.right="20px",d.style.top="20px",d.style.padding="20px",d.style.background="rgba(0,0,0, 0.5)",d.style.width="200px",d.style.color="#fff",d.style.zIndex="9999999",d.appendChild(e),document.body.appendChild(d),c.setAttribute("type","text/javascript"),c.setAttribute("charset","UTF-8"),c.setAttribute("src","//tng.local:8888/assets/js/linkpost.js?r="+Math.random()),document.documentElement.appendChild(c),c.onload=c.onreadystatechange=function(){var d=c.readyState;d&&"loaded"!==d&&"complete"!==d||(c.onload=c.onreadystatechange=null,b!==window[a].version?alert("This bookmarklet is out of date!"):window[a].init())}})();
+javascript:(function(){var a="w00t!";if(window[a])return window[a].init(),void 0;window[a]={};var b=1.6,c=document.createElement("script"),d=document.createElement("div"),e=document.createTextNode("Yo, traveller! The main script is loading. Please stand by...");d.id="tng-linkpost-wait",d.style.position="absolute",d.style.right="20px",d.style.top="20px",d.style.padding="20px",d.style.background="rgba(0,0,0, 0.5)",d.style.width="200px",d.style.color="#fff",d.style.zIndex="9999999",d.appendChild(e),document.body.appendChild(d),c.setAttribute("type","text/javascript"),c.setAttribute("charset","UTF-8"),c.setAttribute("src","//tng.local:8888/assets/js/linkpost.js?r="+Math.random()),document.documentElement.appendChild(c),c.onload=c.onreadystatechange=function(){var d=c.readyState;d&&"loaded"!==d&&"complete"!==d||(c.onload=c.onreadystatechange=null,console.log(b, window[ a ].version),b!==window[a].version?alert("This bookmarklet is out of date!"):window[a].init())}})();
