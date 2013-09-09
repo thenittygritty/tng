ad2bf891730638dff6960cac67152ee8b52603d0
Kahlil Lechelt <hello@kahlil.co>
2013-09-09 09:02:53 +0200

    Transfer changes from older toparticle branch.

--- site/templates/home.php
+++ site/templates/home.php
@@ -3,6 +3,20 @@
 // Show blog posts in decending order
 $articles = $page->children()->visible()->flip();
 
+function get_latest_articles($articles) {
+	$index = 0;
+	$last_three_articles = array();
+	foreach ($articles as $article) {
+		if($article->template() == 'article' && $index < 3) {
+			$last_three_articles[] = $article;
+			$index++;
+		}
+	}
+	return $last_three_articles;
+}
+
+$last_three_articles = get_latest_articles($articles);
+
 // Show posts of categories if requested
 if (param('category')):
 	$articles = $articles->filterBy('categories', urldecode(param('category')), ',')->paginate(5); ?>
@@ -11,7 +25,47 @@ if (param('category')):
 	// Show all articles
 	$articles = $articles->paginate(15);
 endif;
+?>
 
+<article class="article-featured">
+	<header class="article-header">
+		<h1>
+			<a href="<?php echo $last_three_articles[0]->url(); ?>">
+				<?php echo html($last_three_articles[0]->title()) ?>
+			</a>
+		</h1>
+	</header>
+	<?php echo kirbytext($last_three_articles[0]->excerpt()); ?>
+	<p><a href="<?php echo $last_three_articles[0]->url() ?>">Read more…</a></p>
+</article>
+
+<div class="article-teasers">
+	<article class="article-featured">
+		<header class="article-header">
+			<h1>
+				<a href="<?php echo $last_three_articles[1]->url(); ?>">
+					<?php echo html($last_three_articles[1]->title()) ?>
+				</a>
+			</h1>
+		</header>
+		<?php echo kirbytext($last_three_articles[1]->excerpt()); ?>
+		<p><a href="<?php echo $last_three_articles[1]->url() ?>">Read more…</a></p>
+	</article>
+
+	<article class="article-featured">
+		<header class="article-header">
+			<h1>
+				<a href="<?php echo $last_three_articles[2]->url(); ?>">
+					<?php echo html($last_three_articles[2]->title()) ?>
+				</a>
+			</h1>
+		</header>
+		<?php echo kirbytext($last_three_articles[2]->excerpt()); ?>
+		<p><a href="<?php echo $last_three_articles[2]->url() ?>">Read more…</a></p>
+	</article>
+</div>
+
+<?php
 if ($articles && $articles->count()):
 	foreach ($articles as $article):
 		// Check if it is a link post
